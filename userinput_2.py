ba42b01bcc22a50fbdce36319d29fc22975c5bf1
Bristi Maharjan <bristimah@gmail.com>
2025-03-03 23:19:13 +0545

    p1

---
+++ userinput_2.py
@@ -0,0 +1,6 @@
+name=input("What is your name ?: ")
+age = int(input("How old are you ?:"))
+#age=int(age)
+age=age+1
+print(f"Hello {name}")
+print(f"You are of {age} years old")
---
+++ variables_1.py
@@ -0,0 +1,17 @@
+#Strings
+first_name = "Bristi"
+print(first_name)
+print("first_name")
+print(f"Hello {first_name}")
+#Integers
+age=25
+print (f"i am {age} years old")
+#floating
+price=10.99
+print(f'The price is ${price}')
+#boolean
+is_student = True
+if is_student:
+    print("you are student")
+else:
+    print("you are not student")
