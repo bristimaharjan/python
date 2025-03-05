e4fc8c3f7fe4a58df5a760d8604ce6a1a7c5c0af
Bristi Maharjan <bristimah@gmail.com>
2025-03-05 21:28:53 +0545

    tuples

---
+++ tupple_3.py
@@ -0,0 +1,22 @@
+fruits=('apple','orange','banana','coconut','apple')
+#print(dir(fruits))#method the list can perform 
+#print(help(fruits))#to print description of the methods and attributes and whta they can do
+#print(len(fruits))#returns length of the collection
+#print("apple"in fruits)#returns boolean value used to see if the collection contain the value 
+
+"""fruits[0]="pineapple"#change the value
+for fruit in fruits:
+ print(fruit)# iterate list"""
+#fruits.remove("apple")
+#fruits.clear()
+#print(fruits.index("apple")) returns the index of the value 
+#print(fruits.count("apple")) counts number of apple
+#fruits.add("pineaaple")
+#fruits.pop()
+#for fruit in fruits:
+ #print(fruit) iterate list
+#print(fruits.index("apple")) returns the index of the value 
+#print(fruits.count("apple")) counts number of apple
+print(fruits)
+
+
