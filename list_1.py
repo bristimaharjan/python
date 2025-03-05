d5f3ab08b4ae1002fca9f2e3d1ac9831fbfd69f0
Bristi Maharjan <bristimah@gmail.com>
2025-03-05 21:21:12 +0545

    list

---
+++ list_1.py
@@ -0,0 +1,26 @@
+fruits=['apple','orange','banana','coconut']
+#print(fruits)
+#print(fruits[0]) print ...index
+#print(fruits[:3])for 0 to 3 
+#print(fruits[::2]) steps 
+#for fruit in fruits:
+ #print(fruit) iterate list
+#print(dir(fruits))#method the list can perform 
+#print(help(fruits))#to print description of the methods and attributes and whta they can do
+#print(len(fruits))#returns length of the collection
+#print("apple"in fruits)#returns boolean value used to see if the collection contain the value 
+
+"""fruits[0]="pineapple"#change the value
+for fruit in fruits:
+ print(fruit)# iterate list"""
+#fruits.append("pinepplae") add to the last of the list
+#fruits.remove("apple")
+#fruits.insert(0,"pineapple") insert in said index
+#fruits.sort() sort in ascending order
+#fruits.reverse() reverse
+#fruits.clear()
+#print(fruits.index("apple")) returns the index of the value 
+#print(fruits.count("apple")) counts number of apple
+print(fruits)
+
+
