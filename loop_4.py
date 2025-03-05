ac641a7aa89e61912b1f12b18cc3268a279e0685
Bristi Maharjan <bristimah@gmail.com>
2025-03-05 20:37:24 +0545

    loop

---
+++ loop_4.py
@@ -0,0 +1,23 @@
+#name=input("Enter your name :")
+#while name ==" ":
+ #   print("You didnot write the name")
+  #  name=input("Enter your name :")
+   # print(f"Hello{name}")
+"""
+age=int(input("enter your age :"))
+while age>18:
+    print(f"You can vote as your age is {age} ")
+else:
+    print("you cannot vote ")"""
+"""
+food=input("Enter the food you want")
+while not food =="q":
+    print(f"You like {food}")
+    food=input("enter another food you like (q to quit): ")
+    print("bye")"""
+
+num=int(input("Enter a number between 1-10"))
+while num<1 or num>10:
+    print(f"{num} is not valid")
+    num=int(input("Enter a number between 1-10"))
+    print(f"Your number is {num}")
