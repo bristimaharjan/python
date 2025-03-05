588e0da05c2be4bce7be996a0e1f8cd4849b3fde
Bristi Maharjan <bristimah@gmail.com>
2025-03-05 20:57:25 +0545

    for loop

--- loop_4.py
+++ loop_4.py
@@ -36,8 +36,17 @@ credit_card ="12345-678-9103"
 for x in credit_card:
    print(x)
  """
+"""
 for x in range (1,21):
     if x==13:
         continue #break
     else:
-        print(x)
+        print(x)"""
+rows = int(input("Enter the number of rows: "))
+columns = int(input("Enter the number of columns: "))
+symbol = input("Enter the symbol: ")
+
+for x in range(rows):
+    for y in range(columns):
+        print(symbol, end="")
+    print()  # Move to the next line after each row
