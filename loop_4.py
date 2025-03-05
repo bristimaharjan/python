c666bf8ca64b40548e99ae28996fc8cb3ac86d23
Bristi Maharjan <bristimah@gmail.com>
2025-03-05 20:45:18 +0545

    for loop

--- loop_4.py
+++ loop_4.py
@@ -15,9 +15,29 @@ while not food =="q":
     print(f"You like {food}")
     food=input("enter another food you like (q to quit): ")
     print("bye")"""
+"""
 
 num=int(input("Enter a number between 1-10"))
 while num<1 or num>10:
     print(f"{num} is not valid")
     num=int(input("Enter a number between 1-10"))
-    print(f"Your number is {num}")
+    print(f"Your number is {num}")"""
+"""
+for x in range(1,5):
+    print(x)
+
+for x in reversed(range(1,5)):
+    print(x)
+
+for x in reversed(range(1,5,2)):
+ print(x)"""
+"""
+credit_card ="12345-678-9103"
+for x in credit_card:
+   print(x)
+ """
+for x in range (1,21):
+    if x==13:
+        continue
+    else:
+        print(x)
